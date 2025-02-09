e11b238996aa71e05b9ca5d9cfc173d17509fc7f
elifkorkut <61417774+elifkorkut@users.noreply.github.com>
2025-02-09 21:55:37 +0300

    feat: config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/elifkorkut.github.io/',
   plugins: [vue()],
 })
 
+
+/*
 module.exports ={
   publicPath: './elifkorkut.github.io/ '
-}
+}*/
